7a977169371dd4950000c4b525bcdaf2ec926244
Chornoslav <khadai.yaroslav@gmail.com>
2019-08-07 23:00:26 +0300

    rename functions, add rocket moving during acceleration, add correct keyUp resets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 $(document).ready(function () {
     // prevent transition runing, IE fix - disable transition on preloading
@@ -6,29 +6,38 @@ $(document).ready(function () {
 
     var root = document.documentElement;
     var body = document.querySelector('body');
+    var rocket = document.querySelector('.rocket');
 
     var position = 10;
     var velocity;
     function defaultVelocity() {
         velocity = 4;
     }    defaultVelocity();
-    function multi() {
+    function changeFlameAnimationSpeed() {
         root.style.setProperty('--multiplier', 0 + "s");
-    }    multi();
+    }    changeFlameAnimationSpeed();
 
     body.addEventListener('keydown', function (e) {
         //console.log(e);
+        root.style.setProperty('--boost-animation', 0.7 + 's');
         if(e.keyCode === 39 || e.keyCode === 68) {
+            //rotate right
             root.style.setProperty('--angle', 20 + "deg");
         } else if (e.keyCode === 65 || e.keyCode === 37) {
+            //rotate left
             root.style.setProperty('--angle', -20 + "deg");
         }
+
         if (e.keyCode === 87 || e.keyCode === 38) {
+            //increase speed
             velocity = 10;
             root.style.setProperty('--multiplier', -.5 + "s");
+            root.style.setProperty('--boost', 0);
         } else if (e.keyCode === 40 || e.keyCode === 83) {
+            //decrease speed
             velocity = 2;
             root.style.setProperty('--multiplier', .5 + "s");
+            root.style.setProperty('--boost', 24 + '%');
         }
     });
     body.addEventListener('keyup', function (e) {
@@ -36,25 +45,34 @@ $(document).ready(function () {
             root.style.setProperty('--angle', 360 + "deg");
             setTimeout(function() {
                 root.style.setProperty('--angle-animation', 0 + "s");
-                stars();
+                resetStarsRotationAngle();
                 setTimeout(function() {
-                    angle();
+                    fullRotationTransition();
                 }, 100);
             }, 1000);
             return
         }
-        stars();
-        defaultVelocity();
-        multi();
+
+        //reset to default values
+        if(e.keyCode === 39 || e.keyCode === 68 || e.keyCode === 65 || e.keyCode === 37) {
+            //rocket angle
+            resetStarsRotationAngle();
+        }
+        if (e.keyCode === 87 || e.keyCode === 38 || e.keyCode === 40 || e.keyCode === 83) {
+            //rocket speed
+            changeFlameAnimationSpeed();
+            defaultVelocity();
+            rocketNitro();
+        }
     });
 
-    function stars() {
+    function resetStarsRotationAngle() {
         root.style.setProperty('--angle', 0);
-    }    stars();
+    }    resetStarsRotationAngle();
 
-    function angle() {
+    function fullRotationTransition() {
         root.style.setProperty('--angle-animation', 1 + "s");
-    }    angle();
+    }    fullRotationTransition();
 
     function animateStars() {
         position = position + velocity;
@@ -62,4 +80,8 @@ $(document).ready(function () {
         requestAnimationFrame(animateStars);
     }
     animateStars();
+
+    function rocketNitro() {
+        root.style.setProperty('--boost', 14 + "%");
+    } rocketNitro()
 });
