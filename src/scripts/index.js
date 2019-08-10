d79614648bff6c1204a0257b667a39a3be263921
Chornoslav <khadai.yaroslav@gmail.com>
2019-08-11 00:29:34 +0300

    add explosion effect onSpace

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,15 +9,19 @@ $(document).ready(function () {
     var root = document.documentElement;
     var body = document.querySelector('body');
     var rocket = document.querySelector('.rocket');
+    var rocketSVG = document.querySelector('.rocket svg');
     var platform = document.querySelector('.platform');
     var spinPlatform = document.querySelector('.spinPlatform');
+    var explosions = document.querySelectorAll('.explsLayer');
     var rotationAngle = 0;
 
     var position = 10;
     var velocity;
+
     function defaultVelocity() {
         velocity = 4;
     }    defaultVelocity();
+
     function changeFlameAnimationSpeed() {
         root.style.setProperty('--multiplier', 0 + "s");
     }    changeFlameAnimationSpeed();
@@ -34,10 +38,7 @@ $(document).ready(function () {
         }
 
         if (e.keyCode === 87 || e.keyCode === 38) {
-            //increase speed
-            velocity = 10;
-            root.style.setProperty('--multiplier', -.5 + "s");
-            root.style.setProperty('--boost', 0);
+            increaseSpeed();
         } else if (e.keyCode === 40 || e.keyCode === 83) {
             //decrease speed
             velocity = 2;
@@ -59,7 +60,14 @@ $(document).ready(function () {
             defaultVelocity();
             rocketNitro();
         }
+        if (e.keyCode === 32) { explosion() }
     });
+
+    function increaseSpeed(){
+        velocity = 10;
+        root.style.setProperty('--multiplier', -.5 + "s");
+        root.style.setProperty('--boost', 0);
+    }
 
     function resetStarsRotationAngle() {
         root.style.setProperty('--angle', 0);
@@ -80,9 +88,52 @@ $(document).ready(function () {
         rotationAngle = rotationAngle + 8;
         if (rotationAngle >= 361){
             rotationAngle = 0;
-            return;
-        }
+            return }
         spinPlatform.style.transform = 'rotate(' + rotationAngle + 'deg)';
         requestAnimationFrame(rotateRocket);
+    }
+    function explosion() {
+        body.classList.add('explosion');
+        var animationDuration = 700;
+        setTimeout(function () {
+                body.classList.remove('explosion');
+                //hide rocket
+                rocketSVG.style.opacity = 0;
+                animateExplosion();
+                toggleRocket();
+            } , animationDuration);
+
+        var delay = 50;
+        function animateExplosion() {
+            explosions.forEach(function(layer) {
+                delay = delay + 150;
+                //enable each layer
+                layer.style.display = 'block';
+                setTimeout(function () {
+                    layer.style.opacity = 1;
+                }, delay);
+            });
+            setTimeout(function () {
+                //hide layers
+                explosions.forEach(function(exp) {
+                    exp.style.opacity = 0;
+                });
+            }, delay);
+        }
+        function toggleRocket() {
+            setTimeout(function () {
+                rocketSVG.style.opacity = '';
+                increaseSpeed();
+                stopAcceleration();
+            }, delay + 1200);
+        }
+        function stopAcceleration(){
+            var accelerationDuration = 2000;
+            setTimeout( function () {
+                changeFlameAnimationSpeed();
+                defaultVelocity();
+                rocketNitro();
+            }, accelerationDuration);
+        }
     }
 });
